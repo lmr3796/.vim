91c228740f4943aff3047521160547bd039f3e95
cwahbong <cw.ahbong@gmail.com>
2014-02-22 20:20:57 +0800

    Add an easy bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e -u
+
+VIMRC_LINK="${HOME}/.vimrc"
+
+if [ $PWD != "$HOME/.vim" ]
+then
+	echo "This should be executed from .vim directory."
+	exit 1
+fi
+
+if [ -e $VIMRC_LINK ]
+then
+	echo "$HOME/.vimrc already exists."
+else
+	ln -s .vim/vimrc $HOME/.vimrc
+fi
+
+if [ ! -d "bundle" ]
+then
+	mkdir bundle
+fi
+cd bundle
+if [ ! -d "vundle" ]
+then
+	git clone https://github.com/gmarik/vundle.git
+else
+	echo "vundle already cloned."
+fi
